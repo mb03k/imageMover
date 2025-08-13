250d8c0db374b29baf66f2bb1992bc985cf47f0b
Matthes Böttcher <matthesboettcher03@gmail.com>
2025-08-13 17:09:24 +0200

    Feat: File for moving the images itself

--- script/moveImage.py
+++ script/moveImage.py
@@ -1,3 +1,21 @@
 import shutil
+from functionality.pathchecker import pathIsFile
 
-shutil.copyfile(src, dst)
+#shutil.copyfile(src, dst)
+
+def startCopying(paths, conventions):
+    # Datei Zeile für Zeile
+    with open('testdatei.txt', 'r') as file: # TODO: Dateinamen in zentrale Datei verschieben
+        for line in file:
+            #print(line.strip()) # TODO: Prüfung einbauen, ob die eingelesenen Werte die richtige Form haben (je Zeile ein Wert und so)
+
+            infix = line.strip()
+            path = buildFinalPath(paths, infix, conventions)
+
+            if pathIsFile(""):
+                None
+
+def buildFinalPath(paths, infix, conventions):
+    # PATH                     /   PREFIX  INFIX  POSTFIX
+    # /Users/Matthes/Desktop   /   _MB06   231    .ARW           ### BEISPIEL ###
+    print (paths[0] + "/" + conventions[0] + infix + conventions[1])
