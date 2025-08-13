8a1d4f5d6ed9710ac717c07636f1118fda2baeed
Matthes Böttcher <matthesboettcher03@gmail.com>
2025-08-13 17:09:20 +0200

    Fix: Refactor starting points

--- script/main.py
+++ script/main.py
@@ -1,15 +1,35 @@
 import time
-import functionality.general as general
+import functionality.pathcatcher as pathcatcher
+import functionality.nameconvention as nameconvention
+import moveImage as mover
+from variables import GlobalVariables as GV
 from config import MESSAGES
+import config as c
 
-selected_language = "de"
-paths = ["", ""]
+# TODO: 
+# Ordner für ablageort der Bilder auf Wunsch erstellen (falls er nicht gefunden wurde)
 
 def main():
+    svar = GV("", "", "") # TODO: language abfragen
+
     print(MESSAGES["welcome_message"])
 
-    time.sleep(1)
+    time.sleep(0.5)
     
-    paths = general.getValidPaths()
+    paths = pathcatcher.getValidPaths() # gibt array zurüc
+    svar.setPaths(paths)
+    print (svar.paths)
+    # conventions=[prefix, postfix]
+    # conventions = nameconvention.getConventions()
+
+    # mover.startCopying(paths, conventions)
+
+    # p1 = GV("/bin", "/output", "de")
+    # print (p1.paths," " ,p1.selected_language)
+
+    # jetzt prüfen welche bilder man haben will
+    # meine werden als _MB06210.ARW gespeichert
+    # -> präfix & postfix sind (hoffentlich) immer statisc -> _MB06 -> .ARW
+    # zahlen in Liste eingeben, welche dann nacheinander herausgesucht und kopiert werden
 
 main()
