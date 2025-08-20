3335c65d283b7112f77a23cacb14d8e484fed9fb
Matthes Böttcher <matthesboettcher03@gmail.com>
2025-08-20 15:58:19 +0200

    Feat: Add new global variables

--- script/variables.py
+++ script/variables.py
@@ -1,10 +1,13 @@
 
 class GlobalVariables:
-    paths = ["", ""]
-    selected_language = "de"    
+    paths = ["", "", ""]
+    selected_language = "de"
+    INPUTPATH = 0
+    OUTPUTPATH = 1
+    NAMEPATH = 2
 
-    def __init__(self, imageInputPath, imageOutputPath, selectedLanguage):
-        self.paths = [imageInputPath, imageOutputPath]
+    def __init__(self, imageInputPath, imageOutputPath, imageNamePath, selectedLanguage):
+        self.paths = [imageInputPath, imageOutputPath, imageNamePath]
         self.selected_language = selectedLanguage
 
     def setSelectedLanguage(self, selectedLanguage):
