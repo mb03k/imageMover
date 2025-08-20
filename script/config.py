fe20501cfb43cbc5c6cbd067a5a2095bd0fee370
Matthes Böttcher <matthesboettcher03@gmail.com>
2025-08-20 15:58:03 +0200

    Docs: Add config file

--- script/config.py
+++ script/config.py
@@ -1,13 +1,3 @@
-import json
+import functionality.jsonloader as jsonloader
 
-def loadJSON(selected_language):
-    global MESSAGES
-
-    with open('messages.json', 'r', encoding='utf-8') as f:
-        all_messages = json.load(f)
-
-    MESSAGES = all_messages[selected_language]
-    return MESSAGES
-
-
-MESSAGES = loadJSON("de") # SPRACHE NOCH ÄNDERN
+MESSAGES = jsonloader.loadJSON("de") # SPRACHE NOCH ÄNDERN
