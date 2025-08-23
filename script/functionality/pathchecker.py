a80132020c13a018743118ef84e6cb7ecf174835
Matthes Böttcher <matthesboettcher03@gmail.com>
2025-08-23 13:03:17 +0200

    Feat: Add functions to determine if a path is valid

--- script/functionality/pathchecker.py
+++ script/functionality/pathchecker.py
@@ -1,4 +1,15 @@
+
 import os 
+from pathlib import Path
 
 def pathIsValid(path):
     return os.path.exists(path) 
+
+def pathIsFile(path):
+    return Path(path).is_file()
+
+def pathIsValidTextfile(path):
+    endung = os.path.splitext(path)
+    print ("Pfad: ",path)
+    print("split: ",endung)
+    return Path(path).is_file()
